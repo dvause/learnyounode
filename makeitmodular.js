21fecfbdde07a9f6e7ce41cf35a51bbbe2d9da7a
Dave Vause <dvause@gmail.com>
2014-02-24 10:52:37 -0500

    check for err

--- makeitmodular.js
+++ makeitmodular.js
@@ -2,6 +2,10 @@ var filteredls = require('./filteredlsModule.js');
 var args = process.argv;
 
 filteredls(args[2], args[3], function(err, files) {
+	if (err) {
+		return console.error("error: " + err);
+	}
+
   files.forEach(function (file) {
     console.log(file);
   });
