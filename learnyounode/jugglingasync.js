c4c3b64832422298a59dbf914c0dc1fbce27388b
David Vause <dvause@gmail.com>
2014-02-24 21:03:50 -0500

    finished juggling async. ugly...

--- learnyounode/jugglingasync.js
+++ learnyounode/jugglingasync.js
@@ -1,14 +1,32 @@
 var http = require('http'),
-	bl = require('bl'),
-	url = process.argv[2];
+  bl = require('bl'),
+  results = [],
+  count = 0;
 
-http.get(url, function callback(response) {
-	response.pipe(bl(function(err, data) {
-		if (err) {
-			console.error(err);
-		}
-		data = data.toString();
-		console.log(data.length);
-		console.log(data);
-	}));
-});
+
+function get(index) {
+  var url = process.argv[index + 2];
+  http.get(url, function callback(response) {
+    response.pipe(bl(function(err, data) {
+      if (err) {
+        console.error(err);
+      }
+      results[index] = data.toString();
+      count++;
+      if (count == 3) {
+        logResult();
+      }
+    }));
+  });
+}
+
+function logResult() {
+  results.forEach(function(r){
+    console.log(r);
+  })
+}
+
+for (var i = 0; i < 3; i++) {
+  get(i);
+}
+
