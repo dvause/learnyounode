44ee9c824cd8a7fcf57293a241ca3c7277af7a81
Dave Vause <dvause@gmail.com>
2014-02-26 12:05:45 -0500

    lines exercise

--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -1,3 +1,11 @@
 var through = require('through'),
-	split = require('split');
+	split = require('split'),
+	count = 0;
 
+process.stdin.pipe(split()).pipe(through(function(line) {
+		if (count++ % 2) {
+			this.queue(line.toString().toUpperCase() + '\n');
+		} else {
+			this.queue(line.toString().toLowerCase() + '\n');
+		}
+	})).pipe(process.stdout);
